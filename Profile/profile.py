e284521c9f63cabbd167eeca58b2635399e2d7a3
ranchocooper <ranchocooper@gmail.com>
2016-08-16 17:50:43 +0800

    fixed

--- Profile/profile.py
+++ Profile/profile.py
@@ -4,70 +4,40 @@
 # @date     : 2016-07-29 10:11
 # @email    : rancho941110@gmail.com
 
-'''获取所有注册用户的主页链接
+'''
+获取所有注册用户的主页链接
 features:
-- 08.10优化日志信息
 - 08.08生成器代替while循环
-其他: 去掉了随机agent(测试发现直接发包没问题)
-
+- 08.10优化日志信息
 - 08.16多线程模型
+其他:
+不支持自动续爬
+不确定最佳的线程数
+异常日志保存在wrong文件里
+
+Bug:
+- 等待所有线程终止无法捕获键盘中断
 '''
 
 import sys
-import requests
 import json
+import requests
 from lxml import etree
 from Queue import Queue
+from time import sleep
 from threading import Thread
-from time import clock, sleep
 from datetime import datetime
 from itertools import dropwhile
 
-count = 0
+count = 0       # 统计发包数
+users = 0       # 统计用户数
 
-WORK_QUEUE_SIZE = 100
+WORK_QUEUE_SIZE = 100       # 工作队列和线程池大小
 
 sys.stderr = open('wrong', 'a+')
 
-class Downloader():
-    def __init__(self):
-        self.split = '-'
-        self.base = 'http://www.linkedin.com/directory/people-'
-        self.pattern = '//*[@id="seo-dir"]/div/div[5]/div/ul//a/@href'
-
-    # target for Thread, need sub as args
-    def fetch(self):
-        global q, count
-        while True:
-            if q.empty():
-                sleep(0.1)
-            if not q.empty():
-                url = self.base + self.split.join(q.get())
-                try:
-                    res = requests.get(url)
-                    if res.status_code == 200:
-                        with open('all', 'a+') as f:
-                            for refer in self.parse_page(res):
-                                if isinstance(refer, str):
-                                    f.write(refer)
-                                elif isinstance(refer, unicode):
-                                    f.write(refer.encode('utf8'))
-                                else:
-                                    sys.stderr.write('[Unknow encode]: @', url)
-                                    continue
-                            print "saved ", url
-                            count += 1
-                except requests.ConnectionError:
-                    sys.stderr.write('[Requests failed]: %s' % url)
-
-    def parse_page(self, text):
-        selector = etree.HTML(text.content)
-        return selector.xpath(self.pattern)
-
 class Travelor(object):
     def __init__(self, index):
-        self.prog = 0               # flag for progress
-
         self.history = index
         self.alphabet = [i for i in range(1, 27)]
         self.numrange = [i for i in range(1, 101)]
@@ -89,13 +59,50 @@ class Travelor(object):
                 sub[0] = chr(sub[0] + 96)
             yield map(lambda x: str(x), sub)
 
-def write_index(data):
-    """当前记录写入json"""
-    with open('index.json', 'w') as f:
-        json.dump(data, f)
+class Downloader():
+    def __init__(self):
+        self.split = '-'
+        self.base = 'http://www.linkedin.com/directory/people-'
+        self.pattern = '//*[@id="seo-dir"]/div/div[5]/div/ul//a/@href'
+
+    def fetch(self):
+        global q, count, users
+        while True:
+            if q.empty():
+                sleep(0.1)
+            if not q.empty():
+                url = self.base + self.split.join(q.get())
+                try:
+                    res = requests.get(url)
+                    if res.status_code == 200:
+                        # 打开IO接口
+                        with open('all', 'a+') as f:
+                            for refer in self.parse_page(res):
+                                if isinstance(refer, str):
+                                    f.write(refer)
+                                    users += 1
+                                elif isinstance(refer, unicode):
+                                    f.write(refer.encode('utf8'))
+                                    users += 1
+                                else:
+                                    sys.stderr.write('[Unknow encode]: %s\n', url)
+                                    continue
+                                f.write('\n')
+                            print "saved ", url
+                            count += 1
+                except requests.ConnectionError:
+                    sys.stderr.write('[Requests failed]: %s\n' % url)
+                except BaseException, e:
+                    sys.stderr.write('[Unknow Error]: %s\n' % e)
+                except KeyboardInterrupt:
+                    raise KeyboardInterrupt
+
+    def parse_page(self, text):
+        selector = etree.HTML(text.content)
+        return selector.xpath(self.pattern)
 
 def load_index():
-    """从json导入历史位置"""
+    """从json导入开始位置"""
     re = []
     with open('index.json', 'r') as f:
         data = json.load(f)
@@ -106,17 +113,14 @@ def load_index():
 def timelogging(func):
     def wrapper():
         s = (edate - sdate).total_seconds()
-        c = clock()
-        p = (1 - s / c) * 100
         print "START at %32s"               % sdate
         print "ENDED at %32s"               % edate
         print "RUN time used: %26.3f"       % s
-        print "CPU time used: %26.3f"       % c
-        print "IO  time used: %25.3f%%"     % p
         if count:
-            petime = count / s
+            ppage = count / s
+            print "Average  rate: %26.3f/s" % ppage
             print "Requests page: %26d"     % count
-            print "Average  rate: %24.3f/s" % petime
+            print "Fetched users: %26d"     % users
         print
         print "exit, see you..."
         return func()
@@ -138,14 +142,17 @@ if __name__ == '__main__':
     workers = []
     q = Queue(WORK_QUEUE_SIZE)
 
+    # 准备工作队列
     for i in range(WORK_QUEUE_SIZE):
         workers.append(Thread(target=download.fetch))
 
     try:
+        # 启动工作队列
         for work in workers:
             work.daemon = True
             work.start()
 
+        # 迭代目录生成器
         for refer in traverse.traverse():
             while True:
                 if q.full():
@@ -153,8 +160,10 @@ if __name__ == '__main__':
                 else:
                     q.put(refer)
                     break
-
     finally:
-            edate = datetime.now()
-            print "\n\n\n"
-            quitter()
+        # 等待所有线程终止
+        # for work in workers:
+        #     work.join()
+        edate = datetime.now()
+        print "\n\n\n"
+        quitter()
